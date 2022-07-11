5808c2d565a388257f7224aae02a0870b727cfd1
Mike <michael.sypchenko@gmail.com>
2022-07-11 13:13:44 +0300

    dfasdf

---
+++ index.js
@@ -0,0 +1,25 @@
+const { TelegramClient } = require('telegram');
+const { StringSession } = require('telegram/sessions');
+const input = require('input');
+
+const apiId = 12421099;
+const apiHash = '1c8728ba487130e56e65ed1dfe03b21c';
+const code = `1AgAOMTQ5LjE1NC4xNjcuNDEBu4d34t58mUAygUQ8bWhNPcpvUu1mwqUlHaxdkuIJxLseokXYeTsNib/Oa9JX8SPYL4rDbB2rcibD7fUUWBtxqpHmUjc85Cxzl0Uo6BI4AHM0fONdMPjmP3YzPCWP8xqVhDwW68WkMdUJ4k1V95c03FDv9JkNEd0+pwhCd1goxdGGsIz7cD59UnGFSobeK4K6FUvfelYVX1ilhhU2tOYpCshQrohSqkvuffzU/ZJtDBeOCr0mumw33hAnEzLqAdlcDtImSAf/HKPcKP2tQUlWB6XY9wAeDOVPJOdSY+I261cSW6268N43UlRW+/cDxvFne4147hfbGaexmILaELKD/7Q=`;
+let stringSession = new StringSession(code); // fill this later with the value from session.save()
+
+(async () => {
+    console.log("Loading interactive example...");
+    const client = new TelegramClient(stringSession, apiId, apiHash, {
+        connectionRetries: 5,
+    });
+    await client.start({
+        phoneNumber: async () => await input.text("Please enter your number: "),
+        password: async () => '',
+        phoneCode: async () => await input.text("Please enter the code you received: "),
+        onError: (err) => console.log(err)
+    });
+    console.log("You should now be connected.");
+    stringSession = client.session.save();
+    console.log(client.session.save()); // Save this string to avoid logging in again
+    await client.sendMessage("me", { message: "Hello!" });
+})();
