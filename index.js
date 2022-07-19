e18162fab2c4a13c442eb85a2efbbc205f38e455
Mike <michael.sypchenko@gmail.com>
2022-07-19 22:12:48 +0300

    progress

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { TelegramClient, Api } = require('telegram');
+const { NewMessage } = require('telegram/events');
 const { StringSession } = require('telegram/sessions');
 const input = require('input');
 require('dotenv').config();
@@ -8,7 +9,6 @@ const { API_ID, API_HASH, API_CODE } = process.env;
 let stringSession = new StringSession(API_CODE); // fill this later with the value from session.save()
 
 (async () => {
-    console.log("Loading interactive example...");
     const client = new TelegramClient(stringSession, Number(API_ID), API_HASH, {
         connectionRetries: 5
     });
@@ -20,16 +20,21 @@ let stringSession = new StringSession(API_CODE); // fill this later with the val
     });
     console.log("You should now be connected.");
     stringSession = client.session.save();
-    let result;
-    result = await client.invoke(new Api.channels.CreateChannel({
-        title: 'user-and-admin-1ffa', about: 'user-and-admin-1ffa',
-        broadcast: true, megagroup: true, forImport: true
-    }));
-    result = await client.invoke(new Api.channels.InviteToChannel({
-        channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id,
-        users: [ 2093357100 ]
-    }));
-    result = await client.invoke(new Api.channels.LeaveChannel({
-        channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id
-    }));
+    await client.getMe();
+    async function handler(event) {
+        console.log('\n123123', event.message.message, '\n');
+    }
+    client.addEventHandler(handler, new NewMessage({}));//TelegramClient.events.message
+    // let result;
+    // result = await client.invoke(new Api.channels.CreateChannel({
+    //     title: 'user-and-admin-1ffa', about: 'user-and-admin-1ffa',
+    //     broadcast: true, megagroup: true, forImport: true
+    // }));
+    // result = await client.invoke(new Api.channels.InviteToChannel({
+    //     channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id,
+    //     users: [ 2093357100 ]
+    // }));
+    // result = await client.invoke(new Api.channels.LeaveChannel({
+    //     channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id
+    // }));
 })();
