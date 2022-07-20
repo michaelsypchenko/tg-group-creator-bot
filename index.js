21d48a10286e4e8b04a1843d12ae25a0908ec481
Mike <michael.sypchenko@gmail.com>
2022-07-20 12:46:59 +0300

    progress

--- index.js
+++ index.js
@@ -9,32 +9,39 @@ const { API_ID, API_HASH, API_CODE } = process.env;
 let stringSession = new StringSession(API_CODE); // fill this later with the value from session.save()
 
 (async () => {
-    const client = new TelegramClient(stringSession, Number(API_ID), API_HASH, {
-        connectionRetries: 5
-    });
-    await client.start({
-        phoneNumber: async () => await input.text("Please enter your number: "),
-        password: async () => '',
-        phoneCode: async () => await input.text("Please enter the code you received: "),
-        onError: (err) => console.log(err)
-    });
-    console.log("You should now be connected.");
-    stringSession = client.session.save();
-    await client.getMe();
-    async function handler(event) {
-        console.log('\n123123', event.message.message, '\n');
-    }
-    client.addEventHandler(handler, new NewMessage({}));//TelegramClient.events.message
-    // let result;
-    // result = await client.invoke(new Api.channels.CreateChannel({
-    //     title: 'user-and-admin-1ffa', about: 'user-and-admin-1ffa',
-    //     broadcast: true, megagroup: true, forImport: true
-    // }));
-    // result = await client.invoke(new Api.channels.InviteToChannel({
-    //     channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id,
-    //     users: [ 2093357100 ]
-    // }));
-    // result = await client.invoke(new Api.channels.LeaveChannel({
-    //     channel: '-100' + JSON.parse(JSON.stringify(result)).chats[0].id
-    // }));
+	const client = new TelegramClient(stringSession, Number(API_ID), API_HASH, {
+		connectionRetries: 5
+	});
+	await client.start({
+		phoneNumber: async () => await input.text("Please enter your number: "),
+		password: async () => '',
+		phoneCode: async () => await input.text("Please enter the code you received: "),
+		onError: (err) => console.log(err)
+	});
+	console.log("You should now be connected.");
+	stringSession = client.session.save();
+	await client.getMe();
+	async function handler(event) {
+		const groupCreateNotification = JSON.parse(event.message.message);
+		if (
+			!groupCreateNotification
+			|| !groupCreateNotification.manager_tg_id
+			|| !groupCreateNotification.affiliate_tg_id
+		) {
+			return;
+		}
+		const result = await client.invoke(new Api.channels.CreateChannel({
+			title: 'user-and-admin-1ffa', about: 'user-and-admin-1ffa',
+			broadcast: true, megagroup: true, forImport: true
+		}));
+		const createdChanelId = '-100' + JSON.parse(JSON.stringify(result)).chats[0].id;
+		await client.invoke(new Api.channels.InviteToChannel({
+			channel: createdChanelId,
+			users: [
+				Number(groupCreateNotification.manager_tg_id), Number(groupCreateNotification.affiliate_tg_id)
+			]
+		}));
+	   await client.invoke(new Api.channels.LeaveChannel({ channel: createdChanelId }));
+	}
+	client.addEventHandler(handler, new NewMessage({}));//TelegramClient.events.message
 })();
